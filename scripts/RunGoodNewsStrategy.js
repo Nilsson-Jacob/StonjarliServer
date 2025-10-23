e05a0c7339ff7be0d5f2ef0a4a6072f0a21e1858
nilsson-jacob <jacobnilsson97@gmail.com>
2025-10-23 20:50:34 +0200

    sentiment

--- scripts/RunGoodNewsStrategy.js
+++ scripts/RunGoodNewsStrategy.js
@@ -143,7 +143,7 @@ export default async function runGoodNewsStrategy() {
 
     await pool.query(
       `INSERT INTO GOODNEWS (symbol, headline, event) VALUES ($1, $2, $3)`,
-      [news[i].entities[0].symbol, news[i].title, temp]
+      [news[i].entities[0].symbol, news[i].title, temp.event_type]
     );
   }
 
